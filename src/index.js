c4bc0bcfb928b77879466e99b2a4b67e2976b6a4
codingChewie <codingChewie@gmail.com>
2021-01-08 12:02:28 -0600

    working with component trees

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-const App = () => {
-  const [checked, setChecked] = useState(false)
+import { FaStar } from 'react-icons/fa'
 
+const createArray = length => [...Array(length)]
+
+const Star = ({ selected = false }) => {
+  return <FaStar color={selected ? 'red' : 'grey'} />
+}
+
+const StarRating = ({ totalStars = 5 }) => {
+  return createArray(totalStars).map((n, i) => <Star key={i} />)
+}
+
+const App = () => {
   return (
-    <div>
-      <input type="checkbox" value={checked} onChange={() => setChecked(checked => !checked)} />
-      <p>{checked ? 'checked' : 'not checked'}</p>
-    </div>
+    <>
+      <StarRating totalStars={10} />
+      <br />
+      <StarRating />
+    </>
   )
 }
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
