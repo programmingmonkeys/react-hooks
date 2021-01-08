d7dd7a4efe8620dafed83afcec37973d71b388de
codingChewie <codingChewie@gmail.com>
2021-01-08 12:12:52 -0600

    working with the dependency array

--- src/index.js
+++ src/index.js
@@ -1,37 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-import { FaStar } from 'react-icons/fa'
-
-const createArray = length => [...Array(length)]
-
-const Star = ({ selected = false, onSelect }) => {
-  return <FaStar color={selected ? 'red' : 'grey'} onClick={onSelect} />
-}
-
-const StarRating = ({ totalStars = 5 }) => {
-  const [selectedStars, setSelectedStars] = useState(0)
-
-  return (
-    <>
-      {createArray(totalStars).map((n, i) => (
-        <Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1)} />
-      ))}
-      <p>
-        {selectedStars} of {totalStars}
-      </p>
-    </>
-  )
-}
-
 const App = () => {
+  const [name, setName] = useState('Jan')
+
+  useEffect(() => {
+    document.title = `Celebrate ${name}`
+  })
+
   return (
-    <>
-      <StarRating totalStars={10} />
-      <br />
-      <StarRating />
-    </>
+    <section>
+      <p>Congrats {name}</p>
+      <button onClick={() => setName('Will')}>Change Winner</button>
+    </section>
   )
 }
 
